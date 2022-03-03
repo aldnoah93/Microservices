5d556d111d3aa45d63d422a51ef6462838d3724f
mimen093 <15219017+aldnoah93@users.noreply.github.com>
2022-03-02 21:31:00 -0600

    Agregar componente y servicio Books

---
+++ ngBookStore/src/app/books/books.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Books } from './books.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  books: Books[] = [
+    { libroId: 1, titulo: "Algoritmos", descripcion: "Libro basico", autor: "Vaxi Drez", precio: 10},
+    { libroId: 2, titulo: "Angular", descripcion: "Libro basico", autor: "Heli Arcila", precio: 25},
+  ];
+
+  constructor() { }
+}
